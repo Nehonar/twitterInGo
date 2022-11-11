f38f8a104836b394f806669043309663070c4e62
Nehonar <benito.avar@gmail.com>
2022-11-11 15:58:30 +0100

    Add logs annd fix bug in route

--- handlers/handlersRoute.go
+++ handlers/handlersRoute.go
@@ -6,7 +6,7 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
-	"github.com/nehonar/twitteringo/middleware"
+	"github.com/nehonar/twitteringo/middlew"
 	"github.com/nehonar/twitteringo/routers"
 	"github.com/rs/cors"
 )
@@ -17,7 +17,7 @@ HandlersRoute create routes and cors get controll and gift permision allow all
 func HandelersRoute() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/sing-up", middleware.CheckInMongoDB(routers.SignUpUser)).Methods("POST")
+	router.HandleFunc("/signup", middlew.CheckInMongoDB(routers.SignUpUser)).Methods("POST")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
@@ -25,6 +25,5 @@ func HandelersRoute() {
 	}
 
 	handler := cors.AllowAll().Handler(router)
-
 	log.Fatal(http.ListenAndServe(":"+PORT, handler))
 }
